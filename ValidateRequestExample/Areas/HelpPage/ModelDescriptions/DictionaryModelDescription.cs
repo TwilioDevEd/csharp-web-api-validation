f3b641523f1738328a4adab65ab194434d04f23d
David Prothero <david@prothero.com>
2018-01-29 10:50:20 -0800

    Add project files.

---
+++ ValidateRequestExample/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ValidateRequestExample.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ValidateRequestExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ValidateRequestExample.WebApiApplication" Language="C#" %>
